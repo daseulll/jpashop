88f6713df66d0ae54396d9febabb17c5a60b9375
daseulll <nldaseul@gmail.com>
2022-10-18 07:25:02 +0900

    test: lombok 동작 확인

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -3,10 +3,18 @@ package jpabook.jpashop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+
 @SpringBootApplication
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println("data = " + data);
+
+
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
