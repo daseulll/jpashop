efa87d22c6bc92f413e616c5d328dae6c6d163b3
daseulll <nldaseul@gmail.com>
2022-10-31 06:45:33 +0900

    feat: XToOne 관계 fetchType LAZY 설정

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,14 +17,14 @@ public class Order {
     @Column(name="order_id")
     private Long Id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="member_id")
     private Member member;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="delivery_id")
     private Delivery delivery;
 
